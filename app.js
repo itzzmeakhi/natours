bf6e4b13e30ef868b33f4746b0771c5183032993
itzzmeakhi <akhilmallidi.98@gmail.com>
2020-07-08 12:16:06 +0530

    Added PATCH request to update a specific tour data using tourId

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
     // console.log(tour);
 
     if(!tour) {
-        res.status(404).send({
+        res.status(404).json({
             status : 'failed',
             message : 'Invalid Id'
         });
@@ -58,7 +58,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
 
 // Route to POST a tour to the available tours
 
-app.post('api/v1/tours', (req, res) => {
+app.post('/api/v1/tours', (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id : newId }, req.body);
 
@@ -73,6 +73,28 @@ app.post('api/v1/tours', (req, res) => {
         });
     });
 });
+
+// Route to update a specific tour data using tourId and PATCH request
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    const tourId = +req.params.id;
+    const tour = tours.filter(tourElem => tourElem.id === tourId);
+
+    if(!tour) {
+        res.status(404).json({
+            status : 'failed',
+            message : 'Invalid Id'
+        });
+    }
+
+    res.status(200).json({
+        status : 'success',
+        data : {
+            tour : '<Tour Data of Id: .... is updated! />'
+        }
+    });
+});
+
 
 // Listening on specific port
 
