cedefb2e335a65e73ef21f6ce3b0652c7c1e4a94
itzzmeakhi <akhilmallidi.98@gmail.com>
2020-07-10 08:32:48 +0530

    Added Configuration file and dotenv library

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 // Importing the Modules
 
+const dotenv = require('dotenv');
+
+// SET Environment Variables
+
+dotenv.config({ path : './config.env' });
+
 const app = require('./app');
+
+// console.log(process.env);
 
 // SERVER
 
