de9c805335fda8314b61a246fc55545cf653f901
itzzmeakhi <akhilmallidi.98@gmail.com>
2020-07-13 11:20:35 +0530

    Integrated with MongoDB database and pushed a sample tour data to DB

--- server.js
+++ server.js
@@ -18,10 +18,44 @@ const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSW
 mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 })
 .then(con => {
     console.log("DB connected successfully");
+})
+
+// Tour Schema
+
+const tourSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : [true, 'A tour must have a name'],
+        unique : true
+    },
+    rating : {
+        type : Number,
+        default : 4.5
+    },
+    price : {
+        type : Number,
+        required : [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+
+const testTour = new Tour({
+    name : 'The Forest Killer',
+    rating : 4.7,
+    price : 677
+});
+
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log("Error+", err);
 })
 
 
