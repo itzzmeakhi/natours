b06c5538950722348c2a602d09162d7cb1427e29
itzzmeakhi <akhilmallidi.98@gmail.com>
2020-07-21 08:50:37 +0530

    Added a global error handling middleware

--- app.js
+++ app.js
@@ -36,6 +36,30 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
 
+app.use('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status : 'failed',
+    //     message : `Can't find ${req.originalUrl} on this server!`
+    // });
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'failed';
+    err.statusCode = 404;
+
+    next(err);
+});
+
+// Global Error Handling Middleware
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'Error';
+
+    res.status(err.statusCode).json({
+        status : err.status,
+        message : err.message
+    });
+});
+
 // Exporting the Modules
 
 module.exports = app;
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -37,7 +37,6 @@ class APIFeatures {
         } else {
             this.query = this.query.select('-__v');
         }
-
         return this;
     }
   
@@ -45,7 +44,6 @@ class APIFeatures {
         const page = this.queryString.page * 1 || 1;
         const limit = this.queryString.limit * 1 || 100;
         const skip = (page - 1) * limit;
-  
         this.query = this.query.skip(skip).limit(limit);
   
         return this;
